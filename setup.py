992bc87ae605fd18380826f3c2766f4bd6488896
Axel Gard <axel.gard@togethertech.com>
2025-07-22 16:28:01 +0200

    first version that seems to work, still some buys when trying again

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='agentic-tools', 
+    version='0.1.0',
+    author='Axel Gard',
+    author_email='your.email@example.com',
+    description='A toolset for agentic workflows',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/AxelGard/agentic-tools',  
+    packages=find_packages(include=['agentic_tools', 'agentic_tools.*']),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        "langchain-ollama",
+        "langchain",
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+            "black",
+        ]
+    },
+    include_package_data=True,
+)
